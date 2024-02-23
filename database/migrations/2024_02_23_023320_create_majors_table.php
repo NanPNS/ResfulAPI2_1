1ac98ab6dcf2f94d0eea5d4b66c0d707ad0c9402
NanPNS <Panisa7766@gmail.com>
2024-02-23 10:33:10 +0700

    first commit

---
+++ database/migrations/2024_02_23_023320_create_majors_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('majors', function (Blueprint $table) {
+            $table->integer('major_id')->primary();
+            $table->char('name', 50)->nullable(0);
+            $table->char('name_en', 50)->nullable(0);
+            $table->timestamps($precision = 0);	
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('majors');
+    }
+};
